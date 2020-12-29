418fb8d800888d681bfdcd9f789d486ea16db82b
Alma Vidal <alma.vidal@usach.cl>
2020-12-29 02:47:24 -0400

    avance+

--- LAB3_Poo_20655646_VidalBormuth/src/_TDAs/Comun_Res_Preg.java
+++ LAB3_Poo_20655646_VidalBormuth/src/_TDAs/Comun_Res_Preg.java
@@ -1,14 +1,28 @@
 package _TDAs;
 
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Comun_Res_Preg {
 	
 	private static int id = 0;
 	private String autor;
-	Date fechaDePublicacion;
-	int votosAFavor;
-	int votosEnContra;
-	int reportes;
+	private Date fechaDePublicacion;
+	private int votosAFavor;
+	private int votosEnContra;
+	private int reportes;
+	private List<Etiqueta> etiquetas;
+	
+	public Comun_Res_Preg(String autor, Date fechaDePublicacion, int votosAFavor, int votosEnContra, int reportes,
+			List<Etiqueta> etiquetas) {
+		super();
+		this.autor = autor;
+		fechaDePublicacion = new Date();
+		this.votosAFavor = votosAFavor;
+		this.votosEnContra = votosEnContra;
+		this.reportes = reportes;
+		etiquetas = new ArrayList<>();
+	}
 
 }
